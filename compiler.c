9fcfd351c58fabc74605ee13d3d135d5410c828c
ImGabreuw <imgabreuw.dev@gmail.com>
2025-09-09 21:24:23 -0300

    feat: add .gitignore and initial example Pascal program

--- compiler.c
+++ compiler.c
@@ -3,6 +3,30 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello, World!\n");
+    if (argc < 2)
+    {
+        fprintf(stderr, "Source code file not specified. Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+
+    if (file == NULL)
+    {
+        perror("Source code file not found.");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("--- Conteúdo do arquivo: %s ---\n", argv[1]);
+
+    int c;
+    while ((c = fgetc(file)) != EOF)
+    {
+        putchar(c);
+    }
+
+    printf("\n--- Fim do arquivo ---\n");
+
+    fclose(file);
     return EXIT_SUCCESS;
 }
