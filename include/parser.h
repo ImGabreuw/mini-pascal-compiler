55cb4a2fe9e5fe2f95aa9708bfa339af1c10404a
ImGabreuw <imgabreuw.dev@gmail.com>
2025-09-27 17:28:37 -0300

    feat: add parser function declarations for program structure

--- include/parser.h
+++ include/parser.h
@@ -9,4 +9,42 @@ void parser_parse();
 
 void parser_cleanup();
 
+void parser_parse_program();
+
+void parser_parse_block();
+
+void parser_parse_variable_declaration_part();
+
+void parser_parse_variable_declaration();
+
+void parser_parse_type();
+
+void parser_parse_subroutine_declaration_part();
+
+void parser_parse_procedure_declaration();
+
+void parser_parse_function_declaration();
+
+void parser_parse_statement_part();
+
+void parser_parse_expression();
+
+void parser_parse_simple_expression();
+
+void parser_parse_term();
+
+void parser_parse_factor();
+
+void parser_parse_relational_operator();
+
+void parser_parse_adding_operator();
+
+void parser_parse_multiplying_operator();
+
+void parser_parse_sign();
+
+void parser_parse_variable();
+
+void parser_parse_constant();
+
 #endif // PARSER_H
